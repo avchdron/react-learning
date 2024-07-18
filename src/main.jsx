6e04c86c8aeb941da8f339c82080dba9f5a35e8e
avchdron <avchdron@gmail.com>
2024-07-18 22:39:53 +0300

    lesson1

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { restaurants } from './mock.js'
+
+const rootElm = document.getElementById('root');
+const rootReact = ReactDOM.createRoot(rootElm);
+rootReact.render(
+  restaurants.map((Item) => (
+    <div>
+      <h2>{Item.name}</h2>
+      <h3>Меню</h3>
+      <ul>
+        {Item.menu.map((Elm) => (
+          <li>{Elm.name} : ${Elm.price}</li>
+        ))}
+      </ul>
+      <h3>Отзывы</h3>
+      <ul>
+        {Item.reviews.map((review) => (
+            <li> {review.user} ({review.rating}): {review.text} </li>
+        ))}
+      </ul>
+      <hr
+        style={{
+          background: 'black',
+          color: 'black',
+          borderColor: 'black',
+          height: '2px',
+        }}
+      />
+    </div>
+))
+
+);
+
